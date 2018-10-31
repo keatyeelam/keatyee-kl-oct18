d242b798b56d0a1bd642fc1415814627b32ba716
keatyeelam <keatyee.lam@pslgroup.com>
2018-10-31 11:10:52 +0800

    timefunction

---
+++ timeutil.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:07:46 2018
+
+@author: lamkeatyee
+"""
+
+import datetime
+
+currenttime = datetime.datetime.now()
+
+print (currenttime)
+print (currenttime.hour)
+print (currenttime.minute)
+print (currenttime.second)
