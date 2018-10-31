938beeb190bf103c3728b254cd9461b8c09c2289
keatyeelam <keatyee.lam@pslgroup.com>
2018-10-31 10:32:08 +0800

    Date Functions

--- dateuntil.py
+++ dateuntil.py
@@ -9,9 +9,4 @@ Created on Wed Oct 31 10:10:37 2018
 import datetime
 
 currentdate = datetime.date.today()
-print (currentdate)
-print (currentdate.year)
-print (currentdate.month)
-print (currentdate.day)
-
-print (currentdate.strftime('%A, %d %B %Y'))
+print (currentdate.strftime('Please attend our event %A, %B %d in the year %Y'))
