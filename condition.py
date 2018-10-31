5a44ab2bcc017131ac955314310c936d8cb8678e
keatyeelam <keatyee.lam@pslgroup.com>
2018-10-31 11:27:53 +0800

    condition

---
+++ condition.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 11:18:17 2018
+
+@author: lamkeatyee
+"""
+
+answer = input("Would you like express shipping?")
+
+if answer == "yes" :
+    print ("That will be an extra $10")
+    
+print ("Thank you")
