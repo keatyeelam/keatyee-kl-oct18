5ec3db58d1b9b416d0e338c99f0bf974b679cf5c
keatyeelam <keatyee.lam@pslgroup.com>
2018-10-31 10:53:23 +0800

    Birthday

---
+++ birthday.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 10:42:21 2018
+
+@author: lamkeatyee
+"""
+
+import datetime
+
+birthday = input ("What is your birthday? (dd/mm/yyyy):")
+
+birthdate = datetime.datetime.strptime(birthday,"%d/%m/%Y").date()
+
+print ("Your birth month is " + birthdate.strftime('%B'))
