c519e88834cdb8f089bf7b7e81421ae0c1d38d13
keatyeelam <keatyee.lam@pslgroup.com>
2018-11-01 11:30:45 +0800

    loop-while

---
+++ loop-while.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 11:13:44 2018
+
+@author: lamkeatyee
+"""
+
+answer = 0
+
+while answer != 4:
+    answer = int(input("What is 2+2?"))
+
+print("Yes! 2+2 = 4")
