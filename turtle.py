8f5c55e33a779d4b9b472b17d284a841825a9e12
keatyeelam <keatyee.lam@pslgroup.com>
2018-11-01 11:29:14 +0800

    turtle

---
+++ turtle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 10:05:46 2018
+
+@author: lamkeatyee
+"""
+
+import turtle
+for steps in range(4):
+    turtle.forward(100)
+    turtle.right(90)
