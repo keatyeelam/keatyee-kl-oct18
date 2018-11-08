70d96f72141eb356c4ab3f597a7e2d5a20cf2a1a
keatyeelam <keatyee.lam@pslgroup.com>
2018-11-08 11:41:25 +0800

    all

---
+++ PuppyNewGen.py
@@ -0,0 +1,21 @@
+class PuppyNewGen:
+
+    def __init__(self):
+        self.name = []
+        self.color = []
+    
+    def __setitem__(self, name, color):
+        self.name.append(name)
+        self.color.append(color)
+    
+    def __getitem__(self, name):
+        if name in self.name:
+            return self.color[self.name.index(name)]
+        else:
+            return None
+
+dog = PuppyNewGen()
+dog['Max'] = 'brown'
+dog['Ruby'] = 'yellow'
+
+print("Max is ", dog['Max'])
--- oops1.py
+++ oops1.py
@@ -9,9 +9,18 @@ class puppy:
     
     def bark(self):
         print ("I am", self.color, self.name)
+    
+    def __str__(self):
+        ret = "Puppy Object\n"
+        ret += "name:" + self.name + "\n"
+        return ret
 
 puppy1 = puppy("Max","brown")
 puppy1.bark()
 
 puppy2 = puppy("Ruby","black")
 puppy2.bark()
+
+#print(dir(puppy2))
+
+print(puppy2)
