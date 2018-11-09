d7fe60bb54baded794d60fa7e40cdb8d9e7bcaf5
keatyeelam <keatyee.lam@pslgroup.com>
2018-11-09 11:46:51 +0800

    search

--- regex2.py
+++ regex2.py
@@ -1,20 +1,26 @@
 import re
 hand = open('mbox-short.txt')
 
-for line in hand:
-    line = line.rstrip()
+#for line in hand:
+    #line = line.rstrip()
     #if line.find('From:') >= 0 :
     #    print(line)
 
     #if re.search('^X-\S+:',line):
     #    print(line)
 
-    if re.search('[A-Z]+[0-9]+',line):
-        print(line)
+    #if re.search('[A-Z][0-9]+',line):
+    #   print(line)
 
     #print(re.findall('[0-9]+',line))
+
+    #print(re.findall('^F.+:',line))
 
 #x = 'My 2 favourite numbers are 19 and 42'
 #y = re.findall('[0-9]+',x)
 
 #print(y)
+
+#x = 'From: Using the : character'
+#print(re.findall('^F.+:',x)) #greedy match
+#print(re.findall('^F.+?:',x)) #non-greedy match
