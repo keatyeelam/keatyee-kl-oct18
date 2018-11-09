1e166ed87cda7c0f299a97feb2b7e84478381c22
keatyeelam <keatyee.lam@pslgroup.com>
2018-11-09 11:31:12 +0800

    search

---
+++ regex2.py
@@ -0,0 +1,20 @@
+import re
+hand = open('mbox-short.txt')
+
+for line in hand:
+    line = line.rstrip()
+    #if line.find('From:') >= 0 :
+    #    print(line)
+
+    #if re.search('^X-\S+:',line):
+    #    print(line)
+
+    if re.search('[A-Z]+[0-9]+',line):
+        print(line)
+
+    #print(re.findall('[0-9]+',line))
+
+#x = 'My 2 favourite numbers are 19 and 42'
+#y = re.findall('[0-9]+',x)
+
+#print(y)
